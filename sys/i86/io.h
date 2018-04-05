a31f2cec70752d04b335b2fcf90b9ae8fc2cb122
Warner Losh <imp@freebsd.org>
2018-04-04 22:48:15 -0600

    Register definition

---
+++ sys/i86/io.h
@@ -0,0 +1,19 @@
+#define FRAME_AX	0
+#define FRAME_BX	1
+#define FRAME_CX	2
+#define FRAME_DX	3
+#define FRAME_SI	4
+#define FRAME_DI	5
+#define FRAME_FR	6
+#define FRAME_BP	7
+#define FRAME_DS	8
+#define FRAME_ES	9
+#define FRAME_SS	10
+#define FRAME_SP	11
+#define FRAME_CS	12
+#define FRAME_IP	13
+
+#define FRAME_WORDS     14
+
+#ifndef __ASSEMBLER__
+#endif /*  __ASSEMBLER__ */
